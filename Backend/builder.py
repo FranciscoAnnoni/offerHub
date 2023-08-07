0b09853c3557bc9c7055ceab254888b97c6ef2ef
Juan Pereira <juan.mt.pereira@gmail.com>
2023-08-07 20:23:56 -0300

    Template inicial para carga de bd

---
+++ Backend/builder.py
@@ -0,0 +1,32 @@
+import firebase_admin
+from firebase_admin import credentials, db
+
+cred = credentials.Certificate("offerhub-proyectofinal-firebase-adminsdk-szyk2-b5857b6480.json")
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://offerhub-proyectofinal-default-rtdb.firebaseio.com/'})
+
+class Promocion:
+    def __init__(self, nombre, banco):
+        self.nombre = nombre
+        self.banco = banco
+
+def escribirDB(clase, datos):
+    ref = db.reference("/" + clase)
+    for dato in datos:
+        ref.push({'nombre': dato.nombre, 'banco': dato.banco})
+
+# Crear instancias de Promocion
+"""
+promo1 = Promocion("Sofi", "Galicia")
+promo2 = Promocion("Axel", "Santander")
+promociones = [promo1, promo2]
+
+# Escribir en la base de datos
+escribirDB("Promocion", promociones)
+"""
+
+# Realizar una consulta filtrando por el campo "banco"
+ref = db.reference("/Promocion")
+resultados = ref.order_by_child("nombre").equal_to("Juan").get()
+
+for key, value in resultados.items():
+    print(key, "=>", value)
