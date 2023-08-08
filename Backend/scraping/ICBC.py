487c02130d8699c5d2c905f785460e1bf2e999b4
Axel Grimberg <axelgrimb@gmail.com>
2023-08-08 00:09:11 -0300

    Scraping ICBC: Primer guardado en la DB

--- Backend/scraping/ICBC.py
+++ Backend/scraping/ICBC.py
@@ -1,13 +1,24 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService 
+from webdriver_manager.chrome import ChromeDriverManager 
+import sys
+sys.path.append('../')
+import builder as builder
+sys.path.append('../modelos')
+from Promocion import Promocion
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 
 # Configurar el driver de Selenium (en este caso, utilizaremos Chrome)
-driver = webdriver.Chrome()
+options = webdriver.ChromeOptions() 
+options.add_argument('--headless')
+options.add_argument("--window-size=1920,1200")
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
 # Navegar hasta la página de beneficios 
 
@@ -21,7 +32,7 @@ driver.get('https://www.beneficios.icbc.com.ar/promo')
 #Funcion para scrollear hasta abajo de todo de la pagina de beneficios
 last_height = driver.execute_script("return document.body.scrollHeight")
 
-while True:
+while False:##True:
 
    # Scroll down to the bottom.
    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -61,8 +72,8 @@ for boton in seccion_categorias:
     logo=driver.find_element(By.XPATH, '//div[contains(@class,"detalle-beneficio")]').find_element(By.XPATH, './/div[contains(@class,"brand-logo")]').find_element(By.XPATH, './/img').get_attribute("src")
     print("\tURL Logo: "+logo)
     limits=driver.find_element(By.XPATH, '//div[contains(@class,"detalle-beneficio")]').find_elements(By.XPATH, './/time')
-    vigencia= "-".join(map(lambda x: x.get_attribute("innerHTML"), limits))
-    print("\tVigencia: " + vigencia)
+    vigencia= list(map(lambda x: x.get_attribute("innerHTML"), limits))
+    #print("\tVigencia: " + vigencia)
     try:
         ##urlComercio=str(driver.execute_script('if(document.querySelector(".brand-link")){return document.querySelector(".brand-link").href}else{return ""}'))
         urlComercioBtn=driver.find_element(By.XPATH, '//div[contains(@class,"detalle-beneficio")]').find_element(By.XPATH, './/a[contains(@class,"brand-link")]')
@@ -74,32 +85,8 @@ for boton in seccion_categorias:
         descripcion=driver.find_element(By.XPATH, '//li[contains(@class,"description")]').get_attribute("innerHTML")
     except NoSuchElementException:
         descripcion=""
-    segmentos=driver.find_element(By.XPATH, '//div[contains(@class,"detalle-beneficio")]').find_elements(By.XPATH, './/div[contains(@class,"ticket-group-internal")]')
-    i=0
-    for segmento in segmentos:
-        promos=segmento.find_elements(By.XPATH, './/div[contains(@class,"ticket")]')
-        for promo in promos:
-            if 'isClarificacion' not in promo.get_attribute('class').split():
-                if 'div-descuento' not in promo.find_element(By.XPATH, './/div').get_attribute('class').split():
-                    contenido=promo.find_element(By.XPATH, './/div[contains(@class,"content")]')
-                    i+=1
-                    print("\t\t---Promo "+str(i)+"---")
-                    textoPromo=contenido.find_elements(By.XPATH, './/div')
-                    tituloPromo= " ".join((" ".join(map(lambda x: x.get_attribute("innerText") if "footer" not in x.get_attribute("class").split() else "", textoPromo))).split())
-                    print("\t\tTitulo: "+tituloPromo)
-                    try:
-                        nombreSegmento=contenido.find_element(By.XPATH, './/div[contains(@class,"footer")]').text
-                        print("\t\tSegmento: "+nombreSegmento)
-                    except NoSuchElementException:
-                        nombreSegmento="Todos"
-                        print("\t\tSegmento: "+nombreSegmento)
-                    if 'ticket-payroll' in promo.get_attribute('class').split():
-                        print("\t\tCondicion: SI DEPOSITÁS TU SUELDO EN ICBC")
-            else:
-                clarification=" ".join(promo.get_attribute("innerText").split())
-                if len(clarification) > 0:
-                    descripcion=descripcion+(" | " if len(descripcion) > 0 else "")+clarification
     print("\tDescripcion: "+descripcion)
+    segmentos=driver.find_element(By.XPATH, '//div[contains(@class,"detalle-beneficio")]').find_elements(By.XPATH, './/div[contains(@class,"ticket-group-internal")]')
     tope=boton.find_element(By.XPATH, '//li[contains(@class,"tope")]').get_attribute("innerHTML")
     diasSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
     actives=driver.execute_script('return Array.from(document.querySelectorAll(".days span")).map(function(e){ return e.classList[0];}).join()').split(",")
@@ -130,6 +117,42 @@ for boton in seccion_categorias:
             direccion=suc.text
             if len(direccion) >0:
                 print("\t\t\tDireccion: "+direccion+" - Localidad: "+nombre)
+    i=0
+    for segmento in segmentos:
+        promos=segmento.find_elements(By.XPATH, './/div[contains(@class,"ticket")]')
+        for promo in promos:
+            promocion = Promocion()
+
+            if 'isClarificacion' not in promo.get_attribute('class').split():
+                if 'div-descuento' not in promo.find_element(By.XPATH, './/div').get_attribute('class').split():
+                    contenido=promo.find_element(By.XPATH, './/div[contains(@class,"content")]')
+                    i+=1
+                    print("\t\t---Promo "+str(i)+"---")
+                    textoPromo=contenido.find_elements(By.XPATH, './/div')
+                    tituloPromo= " ".join((" ".join(map(lambda x: x.get_attribute("innerText") if "footer" not in x.get_attribute("class").split() else "", textoPromo))).split())
+                    print("\t\tTitulo: "+tituloPromo)
+                    try:
+                        nombreSegmento=contenido.find_element(By.XPATH, './/div[contains(@class,"footer")]').text
+                        print("\t\tSegmento: "+nombreSegmento)
+                    except NoSuchElementException:
+                        nombreSegmento="Todos"
+                        print("\t\tSegmento: "+nombreSegmento)
+                    if 'ticket-payroll' in promo.get_attribute('class').split():
+                        print("\t\tCondicion: SI DEPOSITÁS TU SUELDO EN ICBC")
+            else:
+                clarification=" ".join(promo.get_attribute("innerText").split())
+                if len(clarification) > 0:
+                    descripcion=descripcion+(" | " if len(descripcion) > 0 else "")+clarification
+            promocion.titulo=titulo+": "+tituloPromo
+            promocion.proveedor="ICBC"
+            promocion.url=url
+            promocion.tope=tope
+            promocion.setearFecha("vigenciaDesde",vigencia[0])
+            promocion.setearFecha("vigenciaHasta",vigencia[1])
+            promocion.dias=dias
+            promocion.tyc=tyc
+            promocion.descripcion=descripcion
+            promocion.guardar()
     driver.execute_script('return document.querySelector(".btn-close").click()')
     time.sleep(1)
     
