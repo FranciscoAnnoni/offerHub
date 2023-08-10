6040230d1e3f5f8462f38bab81e7a45c521fc3e4
Axel Grimberg <axelgrimb@gmail.com>
2023-08-10 18:27:51 -0300

    Builder: Modifico escribirDB para que retorne ID + agrego obtenerIdPorContenido

--- Backend/builder.py
+++ Backend/builder.py
@@ -6,9 +6,25 @@ firebase_admin.initialize_app(cred, {'databaseURL': 'https://offerhub-proyectofi
 
 def escribirDB(clase, datos):
     ref = db.reference("/" + clase)
-    for dato in datos:
-        ref.push(dato)
+    ids_guardados = []
 
+    for dato in datos:
+        new_ref = ref.push(dato)
+        ids_guardados.append(new_ref.key)
+
+    return ids_guardados
+
+def obtenerIdPorContenido(coleccion, campo, valor):
+    ref = db.reference(coleccion)
+    snapshot = ref.get()
+
+    if snapshot:
+        valor_minuscula = valor.lower()
+        for key, data in snapshot.items():
+            if campo in data and (data[campo].lower() == valor_minuscula or valor_minuscula in data[campo].lower()):
+                return key
+
+    return None  # Si no se encontró el documento
 # Crear instancias de Promocion
 """
 promo1 = Promocion("Sofi", "Galicia")
