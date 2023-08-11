3a3c731d436f6ea2e42769477f168a820d2d931c
Juan Pereira <juan.mt.pereira@gmail.com>
2023-08-11 17:07:27 -0300

    agrego lectura de base de datos

--- Frontend/app/src/main/java/com/example/offerhub/leerFirebase.kt
+++ Frontend/app/src/main/java/com/example/offerhub/leerFirebase.kt
@@ -1,7 +1,9 @@
 package com.example.offerhub
 
+import com.google.firebase.database.ValueEventListener
 
-    import android.os.Bundle
+import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,30 +13,85 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.example.offerhub.ui.theme.OfferHubTheme
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 
-class Promocion {
-    val listaProv: MutableList<String> = mutableListOf()
+class LecturaBD {
+    //val listaProv: MutableList<String> = mutableListOf()
 
-    fun obtenerPromocionesConCategoria(categoria: String) {
+    fun obtenerPromocionesConCategoria(categoria: String, callback: (MutableList<String>) -> Unit) {
         val database = FirebaseDatabase.getInstance("https://offerhub-proyectofinal-default-rtdb.firebaseio.com/")
-        val promocionRef = database.getReference("Promocion")
+        val promocionRef = database.getReference("/Promocion")
+        val lista: MutableList<String> = mutableListOf()
+        promocionRef.orderByChild("categoria").equalTo(categoria).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (data in dataSnapshot.children){
+                        val titulo = data.child("titulo").getValue(String::class.java)
+                        Log.d("Promocion", "Titulo: $titulo")
+                        if (titulo != null) {
+                            lista.add(titulo)
+                        }
+                    }
 
-        var query = promocionRef.orderByChild("categoria").equalTo(categoria)
-        var proveedor: String
-        query.get().addOnSuccessListener { dataSnapshot ->
-            for (promoSnapshot in dataSnapshot.children) {
-                proveedor = promoSnapshot.child("proveedor").getValue(String::class.java).toString()
-                println(proveedor)
+                }
+                callback.invoke(lista)
             }
-        }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                callback.invoke(lista)
+            }
+        })
     }
 
+    fun obtenerEntidadesPorNombre(nombre: String, callback: (String) -> Unit) {
+        val database = FirebaseDatabase.getInstance("https://offerhub-proyectofinal-default-rtdb.firebaseio.com")
+        val entidadesRef = database.getReference("/Entidad")
+
+        entidadesRef.orderByChild("nombre").equalTo(nombre).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    val entidadSnapshot = dataSnapshot.children.first()
+                    val tipo = entidadSnapshot.child("tipo").getValue(String::class.java)
+                    if (tipo != null) {
+                        callback.invoke(tipo)
+                    } else {
+                        callback.invoke("Vacio")
+                    }
+                } else {
+                    callback.invoke("Vacio")
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                callback.invoke("Vacio")
+            }
+        })
+    }
+
+
 }
 
-fun main(){
-    val intancia = Promocion()
-    intancia.obtenerPromocionesConCategoria("Juguetes")
 
-}
 
+//EJEMPLOS LLAMADAS A FUNCIONES
+/*
+        var instancia = LecturaBD()
+
+
+        setContentView(R.layout.activity_main)
+
+        val textViewNombre = findViewById<TextView>(R.id.textViewNombre)
+        textViewNombre.text = "Nombre: $nombre"
+        val textViewTipo = findViewById<TextView>(R.id.textViewTipo)
+        instancia.obtenerEntidadesPorNombre(nombre){tipo -> textViewTipo.text = "Tipo: $tipo"}
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+
+        instancia.obtenerPromocionesConCategoria("Entretenimiento") { list ->
+            //que hacer con la lista
+        }
+
+
+ */
