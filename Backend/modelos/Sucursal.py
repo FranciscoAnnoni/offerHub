bd2bc70b66bc1043d8a7efb58f785c7d1d4fea74
Axel Grimberg <axelgrimb@gmail.com>
2023-08-15 19:10:50 -0300

    Fix en sucursales cuando se buscaba ya existentes (agrego buscar por direccion)

--- Backend/modelos/Sucursal.py
+++ Backend/modelos/Sucursal.py
@@ -7,9 +7,8 @@ class Sucursal:
     # idComercio
 
     def guardar(self):
-        idSucursal=builder.obtenerIdPorContenido("Sucursal",{"latitud":self.latitud,"longitud":self.longitud, "idComercio":self.idComercio})
+        idSucursal=builder.obtenerIdPorContenido("Sucursal",{"latitud":self.latitud,"longitud":self.longitud, "idComercio":self.idComercio,"direccion":self.direccion})
         if idSucursal==None:
             return builder.escribirDB("Sucursal",[vars(self)])[0]
         else:
             return idSucursal
-    
