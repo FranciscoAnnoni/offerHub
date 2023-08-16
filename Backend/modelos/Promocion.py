6cc49e3ecc4edf13de0a7c380d8e751565920574
Axel Grimberg <axelgrimb@gmail.com>
2023-08-16 01:01:25 -0300

    Agrego funcion para extraer el numero del porcentaje o cantidad de cuotas

--- Backend/modelos/Promocion.py
+++ Backend/modelos/Promocion.py
@@ -2,6 +2,7 @@ import builder as builder
 from CategoriaPromocion import CategoriaPromocion
 from TipoPromocion import TipoPromocion
 from dateutil.parser import parse
+import re
 
 class Promocion:
     # titulo
@@ -22,7 +23,7 @@ class Promocion:
     def setearCategoria(self, nombreCategoria):
         self.categoria=CategoriaPromocion.obtenerCategoria(nombreCategoria)
 
-    def setearTipoPromocion(self,tituloPromo,tope):
+    def setearTipoPromocion(self,tituloPromo,tope=""):
         self.tipoPromocion=TipoPromocion.obtenerTipoPromocion(tituloPromo,tope)
 
     def guardar(self):
@@ -32,3 +33,17 @@ class Promocion:
             else:
                 return idPromocion
     
+    def obtenerPorcentajeYCantCuotas(self,titulo):
+        promo_info = {"porcentaje": None, "cuotas": None}
+        titulo=titulo.lower()
+        # Buscar porcentaje utilizando expresión regular
+        percent_match = re.search(r'(\d+)\s*%', titulo)
+        if percent_match:
+            promo_info["porcentaje"] = percent_match.group(1)
+        
+        # Buscar cuotas utilizando expresión regular
+        cuotas_match = re.search(r'(\d+)\s*cuotas', titulo)
+        if cuotas_match:
+            promo_info["cuotas"] = cuotas_match.group(1)
+        
+        return promo_info
