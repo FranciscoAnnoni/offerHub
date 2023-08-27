82327db49dd92ea8563d6218b4dd330bfc5c2217
fannoni <fannoni@dblandit.com>
2023-08-27 15:16:29 -0300

    pusheo menu y navegacion

---
+++ app/src/main/java/com/example/offerhub/fragments/shopping/FavFragment.kt
@@ -0,0 +1,10 @@
+package com.example.offerhub.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.example.offerhub.R
+
+
+class FavFragment: Fragment(R.layout.fragment_fav){
+    
+
+}
---
+++ app/src/main/java/com/example/offerhub/fragments/shopping/SearchFragment.kt
@@ -0,0 +1,10 @@
+package com.example.offerhub.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.example.offerhub.R
+
+
+class SearchFragment: Fragment(R.layout.fragment_search){
+    
+
+}
