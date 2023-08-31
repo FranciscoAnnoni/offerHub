629fd62b0addceb6cb65e51df9c0b53b2b0c0d50
Juan Pereira <juan.mt.pereira@gmail.com>
2023-08-31 09:56:19 -0300

    Agrego funcion que obtiene promociones comunes sin tarjetas

--- Frontend/app/src/main/java/com/example/offerhub/Funciones.kt
+++ Frontend/app/src/main/java/com/example/offerhub/Funciones.kt
@@ -5,11 +5,25 @@ import kotlinx.coroutines.*
 
 
 class Funciones {
+
+    val instancia = LecturaBD()
+    val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    //Obtiene las promociones de comercios que aplican a cualq usuario, sin necesidad de tarjetas.
+    suspend fun obtenerPromocionesComunes(): List<Promocion> = coroutineScope {
+        val listaPromos: MutableList<Promocion> = mutableListOf()
+        val promosDeferred = async {
+            instancia.obtenerPromosPorTarjetas("No posee")
+        }
+        val promos = promosDeferred.await() // Esperar a que se completen las promociones
+        if (promos != null) {
+            listaPromos.addAll(promos)
+        }
+        listaPromos
+    }
+
     suspend fun listarPromociones(usuario: Usuario): List<Promocion> = coroutineScope {
         val listaPromos: MutableList<Promocion> = mutableListOf()
-        val instancia = LecturaBD()
-        val coroutineScope = CoroutineScope(Dispatchers.Main)
-
         val deferredPromos = usuario.tarjetas?.map { tarjeta ->
             coroutineScope.async {
                 val promos = tarjeta?.let { instancia.obtenerPromosPorTarjetas(it) }
@@ -78,6 +92,21 @@ class Funciones {
                 }
             } catch (e: Exception) {
                 Log.d("Resultado","ERROR")
+            }
+        }
+
+  ObtenerPromocionesComunes:
+          val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+        coroutineScope.launch {
+            try {
+                var promociones = instancia.obtenerPromocionesComunes()
+                println("Promociones obtenidas:")
+                for (promo in promociones){
+                    promo.titulo?.let { Log.d("promo", it) }
+                }
+            } catch (e: Exception) {
+                println("Error al obtener promociones: ${e.message}")
             }
         }
  */
