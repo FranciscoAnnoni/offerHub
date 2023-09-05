2da9bd01d6bbe1e6db28b414ba346b9ead5740cd
fannoni <fannoni@dblandit.com>
2023-09-05 14:25:29 -0300

    creo pantallas y agrego el login del usuario para que inicie en la pantalla login

--- app/src/main/java/com/example/offerhub/di/AppModule.kt
+++ app/src/main/java/com/example/offerhub/di/AppModule.kt
@@ -1,5 +1,8 @@
 package com.example.offerhub.di
 
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import com.example.offerhub.util.Constants.INTRODUCTION_SP
 import com.google.firebase.auth.FirebaseAuth
 import dagger.Module
 import dagger.Provides
@@ -19,5 +22,10 @@ object AppModule {
     @Provides
     @Singleton
     fun provideFirebaseAuth() = FirebaseAuth.getInstance()
+
+    @Provides
+    fun provideIntroductionSP(
+        application: Application
+    ) = application.getSharedPreferences(INTRODUCTION_SP, MODE_PRIVATE)
 
 }
--- app/src/main/java/com/example/offerhub/fragments/loginRegister/IntroductionFragment.kt
+++ app/src/main/java/com/example/offerhub/fragments/loginRegister/IntroductionFragment.kt
@@ -1,16 +1,26 @@
 package com.example.offerhub.fragments.loginRegister
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.offerhub.R
+import com.example.offerhub.activities.ShoppingActivity
 import com.example.offerhub.databinding.FragmentIntroductionBinding
+import com.example.offerhub.viewmodel.IntroductionViewModel
+import com.example.offerhub.viewmodel.IntroductionViewModel.Companion.ACCOUNT_OPTIONS_FRAGMENT
+import com.example.offerhub.viewmodel.IntroductionViewModel.Companion.SHOPPING_ACTIVITY
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class IntroductionFragment:Fragment(R.layout.fragment_introduction) {
     private lateinit var binding: FragmentIntroductionBinding
+    private val viewModel by viewModels<IntroductionViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,7 +34,27 @@ class IntroductionFragment:Fragment(R.layout.fragment_introduction) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.navigate.collect{
+                when(it){
+                    SHOPPING_ACTIVITY ->{
+                        Intent(requireActivity(), ShoppingActivity::class.java).also { intent ->
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intent)
+                        }
+                    }
+
+                    ACCOUNT_OPTIONS_FRAGMENT -> {
+                        findNavController().navigate(R.id.action_introductionFragment_to_loginFragment)
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
         binding.botonStart.setOnClickListener{
+            viewModel.startButtonClick()
             findNavController().navigate(R.id.action_introductionFragment_to_loginFragment)
         }
     }
