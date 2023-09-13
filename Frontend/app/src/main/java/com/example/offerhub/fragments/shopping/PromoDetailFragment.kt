6f56d89a7031708bb6ef7233a6513c9bc57b5d14
Axel Grimberg <axelgrimb@gmail.com>
2023-09-12 21:37:24 -0300

    Cambios en detalle de promo. Agrego logo, relleno fondo con el color

--- Frontend/app/src/main/java/com/example/offerhub/fragments/shopping/PromoDetailFragment.kt
+++ Frontend/app/src/main/java/com/example/offerhub/fragments/shopping/PromoDetailFragment.kt
@@ -6,15 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.offerhub.Comercio
+import com.example.offerhub.Funciones
 import com.example.offerhub.R
 import com.example.offerhub.databinding.FragmentPromoDetailBinding
+import com.example.offerhub.funciones.obtenerColorMayoritario
 import com.example.offerhub.funciones.removeAccents
 import com.example.offerhub.viewmodel.PromoDetailViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class PromoDetailFragment: Fragment(R.layout.fragment_promo_detail){
     private val args by navArgs<PromoDetailFragmentArgs>()
@@ -41,6 +48,25 @@ class PromoDetailFragment: Fragment(R.layout.fragment_promo_detail){
         binding.apply {
             promoTitulo.text = promocion.titulo
             promoDesc.text = promocion.tyc
+            val coroutineScope = CoroutineScope(Dispatchers.Main)
+            coroutineScope.launch {
+                val logoBitmap = Comercio(
+                    "",
+                    "",
+                    "",
+                    ""
+                ).base64ToBitmap(Funciones().traerLogoComercio(promocion.comercio))
+                if (logoBitmap != null) {
+                    viewPagerProductImages.setImageBitmap(logoBitmap)
+                    val color=obtenerColorMayoritario(logoBitmap)
+                    // Suponiendo que ya tienes el color mayoritario en la variable colorMayoritario
+                    val cardProductImages = view.findViewById<CardView>(R.id.cardProductImages)
+
+// Establece el color de fondo del CardView
+                    cardProductImages.setBackgroundColor(color)
+
+                }
+            }
             for (dia in promocion.dias!!) {
                 val nombreDia= removeAccents(dia)
                 val circleId = resources.getIdentifier("circle$nombreDia", "id", requireContext().packageName)
--- Frontend/app/src/main/java/com/example/offerhub/funciones/formateo.kt
+++ Frontend/app/src/main/java/com/example/offerhub/funciones/formateo.kt
@@ -2,9 +2,42 @@ package com.example.offerhub.funciones
 
 import java.text.Normalizer
 import java.util.regex.Pattern
-
+import android.graphics.Bitmap
+import android.graphics.Color
 fun removeAccents(input: String?): String {
     val normalized = Normalizer.normalize(input, Normalizer.Form.NFD)
     val pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+")
     return pattern.matcher(normalized).replaceAll("")
+}
+
+fun obtenerColorMayoritario(bitmap: Bitmap): Int {
+    val width = bitmap.width
+    val height = bitmap.height
+    val pixels = IntArray(width * height)
+    bitmap.getPixels(pixels, 0, width, 0, 0, width, height)
+
+    // Mapa para contar la ocurrencia de cada color
+    val colorCountMap = HashMap<Int, Int>()
+
+    // Recorre los píxeles de la imagen y cuenta la ocurrencia de cada color
+    for (color in pixels) {
+        if (colorCountMap.containsKey(color)) {
+            colorCountMap[color] = colorCountMap[color]!! + 1
+        } else {
+            colorCountMap[color] = 1
+        }
+    }
+
+    // Encuentra el color con mayor ocurrencia
+    var colorMayoritario = Color.BLACK // Color predeterminado en caso de empate o imagen vacía
+    var maxOcurrencia = 0
+
+    for ((color, ocurrencia) in colorCountMap) {
+        if (ocurrencia > maxOcurrencia) {
+            colorMayoritario = color
+            maxOcurrencia = ocurrencia
+        }
+    }
+
+    return colorMayoritario
 }
