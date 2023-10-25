e31165263717c9b931292fb1414788403d500242
fannoni <fannoni@dblandit.com>
2023-10-25 18:48:51 -0300

    agrego cambios con facu

--- Frontend/app/src/main/java/com/example/offerhub/fragments/settings/NotificarErrorFragment.kt
+++ Frontend/app/src/main/java/com/example/offerhub/fragments/settings/NotificarErrorFragment.kt
@@ -8,16 +8,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.example.offerhub.R
 import com.example.offerhub.databinding.FragmentLegalsBinding
 import com.example.offerhub.databinding.FragmentNotificarErrorBinding
+import com.example.offerhub.util.Resource
 import com.example.offerhub.util.hideBottomNavigationView
 import com.example.offerhub.util.showBottomNavigationView
+import com.example.offerhub.viewmodel.LoginViewModel
+import com.example.offerhub.viewmodel.NotificarErrorViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class NotificarErrorFragment : Fragment() {
     private lateinit var binding: FragmentNotificarErrorBinding
+    private val viewModel by viewModels<NotificarErrorViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +43,46 @@ class NotificarErrorFragment : Fragment() {
 
 
         // ver con facu actividad
+        binding.apply {
+            buttonSend.setOnClickListener {
+                val notificacionDeError = edNotificacionDeError.text.toString().trim()
+                viewModel.enviarNotificacionDeError(notificacionDeError)
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.notificacionExitosa.collect {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.buttonSend.startAnimation()
+                    }
+                    is Resource.Success -> {
+
+
+                        binding.buttonSend.revertAnimation()
+                        binding.buttonSend.setBackgroundResource(R.drawable.rounded_button_background)
+
+                        findNavController().navigate(R.id.action_notificarErrorFragment_to_profileFragment)
+
+
+                        Snackbar.make(
+                            requireView(),
+                            "Su comentario se a enviado correctamente",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+
+                    }
+                    is Resource.Error -> {
+                        Snackbar.make(
+                            requireView(),
+                            "Error: ${it.message}",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
 
         binding.imageCloseLegals.setOnClickListener {
             findNavController().navigateUp()
